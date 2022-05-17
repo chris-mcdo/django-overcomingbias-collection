8e56ac3a2321713afad17c95eb63a28bdc2bf77a
chris-mcdo <cmcdonal33@gmail.com>
2022-05-17 22:33:10 +0100

    update production settings

--- obcollection/settings/production.py
+++ obcollection/settings/production.py
@@ -7,11 +7,12 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SITE_HOST_NAME = os.getenv("SITE_HOST_NAME")
 
 # Debug mode
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [SITE_HOST_NAME]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -21,4 +22,11 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # noqa
     }
+}
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "haystack.backends.simple_backend.SimpleEngine",
+    },
 }
